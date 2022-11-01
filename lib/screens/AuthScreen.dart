efd3516a9ab017aa4ed0e84e1893cb3dba6077fe
Honasp <61389885+Honasp@users.noreply.github.com>
2022-11-01 19:49:23 +0800

    11

---
+++ lib/screens/AuthScreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:ren_application/theme/model_theme.dart';
+import 'package:provider/provider.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ModelTheme>(
+        builder: (context, ModelTheme themeNotifier, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Авторизация'),
+          actions: [
+            IconButton(
+                icon: Icon(themeNotifier.isDark
+                    ? Icons.nightlight_round
+                    : Icons.wb_sunny),
+                onPressed: () {
+                  themeNotifier.isDark
+                      ? themeNotifier.isDark = false
+                      : themeNotifier.isDark = true;
+                })
+          ],
+        ),
+        body: Container(),
+      );
+    });
+  }
+}
